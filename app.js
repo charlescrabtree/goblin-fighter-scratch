e155aa33914745088dcf56e8ff9b479ab28a7d77
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-04 17:50:35 -0700

    java starting to take shape

--- app.js
+++ app.js
@@ -5,8 +5,51 @@ const arnImgEl = document.querySelector('#arn-img');
 const form = document.querySelector('form');
 const goblinListEl = document.querySelector('.goblins');
 // let state
-
+let deadGobs = 0;
+let arnoldHP = 200;
+let goblins = [
+    { name: 'Samantha', hp: 4},
+    { name: 'Noi', hp: 6},
+    { name: 'Kalia', hp: 20}
+];
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const gobName = data.get('gob-gob-name');
+
+    const newGob = {
+        name: gobName,
+        hp: Math.ceil(Math.random() * 15),
+    };
+
+    goblins.push(newGob);
+
+    displayGoblins();
+});
+
+function displayGoblins() {
+    goblinListEl.textContent = '';
+
+    for (let goblin of goblins) {
+
+        const goblinEl = renderGoblin(goblin);
+
+        if (goblin.hp > 0) {
+            goblinEl.addEventListener('click', () => {
+                if (Math.random() < .25) {
+                    goblin.hp--;
+                    alert('you hit poor wittle ' + goblin.name);
+                } else {
+                    alert ('Shame on you! You tried to hit ' + goblin.name + ' but you missed. Big meanie.');
+                }
+            }
+          }
+
+          goblinListEl.append(goblinEl);
+    }
+}
+
+displayGoblins();
